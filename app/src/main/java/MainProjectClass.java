2a0d88a2182fb394807c6a11fc1491280719f6eb
dsauce5 <dsauce5@illinois.edu>
2018-12-08 20:46:51 -0600

    First Committ Making sure everything works

--- app/src/main/java/MainProjectClass.java
+++ app/src/main/java/MainProjectClass.java
@@ -1,3 +1,9 @@
 public class MainProjectClass {
     /** This whwere all the work is going to be at Oscar if you can see this text me
+     * This should work completely
+     */
+    public static void main(String[] unused) {
+        System.out.println("I am going to do Math homework and then work on this. Text me if you " +
+                "get this message");
+    }
 }
