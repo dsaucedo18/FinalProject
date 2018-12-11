e5dbee91b419b86eb2f6c27a4c4fa3db68728df6
ozepeda2 <ozepeda2@illinois.edu>
2018-12-10 22:38:42 -0600

    fds

--- app/src/main/java/MainProjectClass.java
+++ app/src/main/java/MainProjectClass.java
@@ -1,5 +1,18 @@
-public class MainProjectClass {
-    /** This whwere all the work is going to be at Oscar if you can see this text me
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.danny_mac.myapplication.R;
+
+public class MainProjectClass extends AppCompatActivity {
+
+    @Override
+    protected void onCrate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /** This where all the work is going to be at Oscar if you can see this text me
      * This should work completely
      */
     public static void main(String[] unused) {
