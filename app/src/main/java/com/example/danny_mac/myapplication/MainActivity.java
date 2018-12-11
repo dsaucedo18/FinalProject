cf45100cdad3044c9fede8188b8d591f08bfa911
dsauce5 <dsauce5@illinois.edu>
2018-12-11 02:14:25 -0600

    First Committ Making sure everything works

--- app/src/main/java/com/example/danny_mac/myapplication/MainActivity.java
+++ app/src/main/java/com/example/danny_mac/myapplication/MainActivity.java
@@ -5,6 +5,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import static com.android.volley.VolleyLog.TAG;
+
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -16,6 +33,30 @@ public class MainActivity extends Activity implements View.OnClickListener {
         Intent intent = new Intent(MainActivity.this, Leagues.class);
         startActivity(intent);
     }
+
+    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+            Request.Method.GET,
+            "",
+            null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(final JSONObject response) {
+                    Log.d(TAG, response.toString());
+                }
+            }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(final VolleyError error) {
+            Log.w(TAG, error.toString());
+        }
+    }) {
+        @Override
+        public Map<String, String> getHeaders() {
+            Map<String, String>  params = new HashMap<String, String>();
+            params.put("\"X-Auth-Token", "c96d93889bad4c9f87acf3317295e340");
+            Log.d(TAG, params.toString());
+            return params;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
