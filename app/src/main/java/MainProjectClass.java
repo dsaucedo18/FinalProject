d595472b643584604e70ed03cb40d6cb7a301894
ozepeda2 <ozepeda2@illinois.edu>
2018-12-10 22:51:24 -0600

    fds

--- app/src/main/java/MainProjectClass.java
+++ app/src/main/java/MainProjectClass.java
@@ -1,14 +1,32 @@
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.danny_mac.myapplication.Leagues;
 import com.example.danny_mac.myapplication.R;
 
 public class MainProjectClass extends AppCompatActivity {
+    private Button leaguesBtn;
 
     @Override
-    protected void onCrate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        leaguesBtn = (Button) findViewById(R.id.button1);
+        leaguesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFixturesScreen();
+            }
+        });
+    }
+
+    public void openFixturesScreen() {
+        Intent intent = new Intent(this, Leagues.class);
+        startActivity(intent);
     }
 
 
